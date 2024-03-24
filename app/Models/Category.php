e9e40e6f6cbe2270b4a9c781ec08fec218fef0a3
THEVENOUX Jean-Philippe <jp.thevenoux@pm.me>
2024-03-24 10:11:34 +0100

    Divers (#26)
    * feat: add settings link on mobile
    
    * feat: add icon picker lib
    
    * feat: add icon to category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -18,7 +18,7 @@ class Category extends Model
      *
      * @var array<int, string>
      */
-    protected $fillable = ['label', 'credit', 'extra', 'recurrent'];
+    protected $fillable = ['label', 'credit', 'extra', 'recurrent', 'icon'];
 
     public function notes(): HasMany
     {
--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -91,6 +91,11 @@ $logout = function (Logout $logout) {
                 {{ __('Budget') }}
             </x-responsive-nav-link>
         </div>
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('settings')" :active="request()->routeIs('settings')" wire:navigate>
+                {{ __('Settings') }}
+            </x-responsive-nav-link>
+        </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
