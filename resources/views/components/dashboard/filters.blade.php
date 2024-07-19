9e8ed600cfd969f93e2c8b1f0e335a46b5493dcc
THEVENOUX Jean-Philippe <jp.thevenoux@pm.me>
2024-07-19 23:00:54 +0200

    change dashboard filters (#47)

--- resources/views/components/dashboard/filters.blade.php
+++ resources/views/components/dashboard/filters.blade.php
@@ -1,10 +1,10 @@
-@props(['total' => 0, 'year', 'month', 'actions' => '', 'filters' => ''])
+@props(['total' => 0, 'year', 'month', 'actions' => '', 'form' => ''])
 
 @php
     use Carbon\Carbon;
     $currentDate = Carbon::createMidnightDate($year, $month + 1, 1)->locale('fr');
-    $nextDate = $currentDate->copy()->addMonth()->toDateString();
-    $prevDate = $currentDate->copy()->subMonth()->toDateString();
+    $nextDate = $currentDate->copy()->addMonth();
+    $prevDate = $currentDate->copy()->subMonth();
 @endphp
 
 <x-section {{ $attributes }}>
@@ -16,17 +16,23 @@
         {{ $actions }}
     </x-slot:header>
     <div>
-        <div class="flex items-center justify-center mb-2">
-            <x-filament::icon-button icon="heroicon-m-arrow-left" label="Précédent"
-                wire:click="$dispatch('update-filters',{ date: '{{ $prevDate }}'})" />
+        <div class="flex justify-center mb-4">
+            <x-filament::button size="xs" outlined icon="heroicon-m-arrow-left"
+                wire:click="$dispatch('update-filters',{ date: '{{ $prevDate->toDateString() }}'})">
+                {{ Str::ucfirst($prevDate->isoFormat('MMM')) }}
+            </x-filament::button>
 
-            {{-- todo : afficher calendrier pour changer rapidement --}}
-            <span class="mx-4">{{ Str::ucfirst($currentDate->isoFormat('MMMM Y')) }}</span>
+            <x-filament::button size="sm" outlined class="mx-4">
+                {{ Str::ucfirst($currentDate->isoFormat('MMMM Y')) }}
+            </x-filament::button>
 
-            <x-filament::icon-button icon="heroicon-m-arrow-right" label="Suivant"
-                wire:click="$dispatch('update-filters',{ date: '{{ $nextDate }}'})" />
-
-            {{ $filters }}
+            <x-filament::button size="xs" outlined icon="heroicon-m-arrow-right" icon-position="after"
+                wire:click="$dispatch('update-filters',{ date: '{{ $nextDate->toDateString() }}'})">
+                {{ Str::ucfirst($nextDate->isoFormat('MMM')) }}
+            </x-filament::button>
+        </div>
+        <div>
+            {{ $form }}
         </div>
     </div>
 </x-section>
